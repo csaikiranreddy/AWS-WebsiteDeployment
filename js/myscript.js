70a98e95199b9776f426d28754191a02b34ed223
saikiran chamakura <13063473+csaikiranreddy@users.noreply.github.com>
2018-02-03 18:17:26 -0700

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,18 @@
+function mybd() {
+  alert("welcome to Big Data");
+}
+function myjava() {
+  alert("Needs to be updated");
+}
+function myml() {
+  alert("welcome to MachineLearning");
+}
+function mytek() {
+  alert("Needs to be updated");
+}
+function myaws() {
+  alert("Needs to be updated");
+}
+function mybz() {
+  alert("Needs to be updated");
+}
